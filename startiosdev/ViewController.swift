6772ff1c24b7b0602c5c998011ed14f4a8c99144
qiantao <532661800@qq.com>
2016-03-10 11:19:33 +0800

    Create Xcode project
    Start this project, add initial project files.

---
+++ startiosdev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  startiosdev
+//
+//  Created by qiantao on 16/3/10.
+//  Copyright © 2016年 QTTS.Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
