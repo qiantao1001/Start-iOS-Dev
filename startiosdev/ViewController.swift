117f963e832e6ef73b4d5ab3b307c4a35d7d348d
qiantao <532661800@qq.com>
2016-03-14 13:14:20 +0800

    Add LaunchScreen
    LaunchScreen V1.0

--- startiosdev/ViewController.swift
+++ startiosdev/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSThread.sleepForTimeInterval(30.0)
     }
 
     override func didReceiveMemoryWarning() {
